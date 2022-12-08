3e08f6e836dc741d81b0c2984484d7520edccc01
Joaquín Fernández <67400456+Joacker@users.noreply.github.com>
2022-12-08 18:10:31 -0300

    new commit

--- backend/catalogo.py
+++ backend/catalogo.py
@@ -10,23 +10,42 @@ class Catalogo(Service):
         super().__init__("bgrup")
         self.start_service(debug=True)
 
-    def service_function(self, climsg):
+    def service_function(self, climsg, climsg2):
         '''Funcion temporal, sera reemplazada en los distintos servicios'''
         db = session()
         try:
-            #climsg = json.loads(climsg)
-            catalogo = db.query(Bodega).filter(Bodega.stock > 0).all()
-            print("Lista de productos:")
-            list_products = ""
-            index = 0
-            for c in catalogo:
-                if (index == (len(catalogo)-1)):
-                    list_products = list_products + "#ID: "+str(c.id)+" ;; Nombre: "+str(c.nombre_producto)+" ;; Precio: "+str(c.valor_unidad)+" ;; Stock: "+str(c.stock)
-                else:
-                    list_products = list_products + "#ID: "+str(c.id)+" ;; Nombre: "+str(c.nombre_producto)+" ;; Precio: "+str(c.valor_unidad)+" ;; Stock: "+str(c.stock)+","
-                index = index + 1
-            index = 0
-            return list_products
+            climsg = json.loads(climsg)
+            choice = climsg["choice"]
+            if (choice == "1"): 
+                catalogo = db.query(Bodega).filter(Bodega.stock > 0).all()
+                print("Lista de productos:")
+                list_products = ""
+                index = 0
+                for c in catalogo:
+                    if (index == (len(catalogo)-1)):
+                        list_products = list_products + "#ID: "+str(c.id)+" ;; Nombre: "+str(c.nombre_producto)+" ;; Precio: "+str(c.valor_unidad)+" ;; Stock: "+str(c.stock)
+                    else:
+                        list_products = list_products + "#ID: "+str(c.id)+" ;; Nombre: "+str(c.nombre_producto)+" ;; Precio: "+str(c.valor_unidad)+" ;; Stock: "+str(c.stock)+","
+                    index = index + 1
+                index = 0
+                return list_products
+            elif (choice == "2"):
+                ID = climsg2["id"]
+                catalogo = db.query(Bodega).filter(Bodega.stock > 0, Bodega.id == ID).all()
+                print("Lista de productos:")
+                list_products = ""
+                index = 0
+                for c in catalogo:
+                    if (index == (len(catalogo)-1)):
+                        list_products = list_products + "#ID: "+str(c.id)+" ;; Nombre: "+str(c.nombre_producto)+" ;; Precio: "+str(c.valor_unidad)+" ;; Stock: "+str(c.stock)
+                    else:
+                        list_products = list_products + "#ID: "+str(c.id)+" ;; Nombre: "+str(c.nombre_producto)+" ;; Precio: "+str(c.valor_unidad)+" ;; Stock: "+str(c.stock)+","
+                    index = index + 1
+                index = 0
+                return list_products
+            elif (choice == "3"):
+                return "Cerrando catalogo de productos"
+                
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
