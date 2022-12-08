24efd3c0b501c7c664fb97a9094e4c9b5616e963
Joaquín Fernández <67400456+Joacker@users.noreply.github.com>
2022-12-08 16:35:06 -0300

    catalogo service set

--- backend/catalogo.py
+++ backend/catalogo.py
@@ -6,7 +6,7 @@ from time import sleep
 
 class Catalogo(Service):
     def __init__(self):
-        print("Servicio de grupos de usuarios")
+        print("Servicio de catalogo de productos")
         super().__init__("bgrup")
         self.start_service(debug=True)
 
@@ -14,10 +14,12 @@ class Catalogo(Service):
         '''Funcion temporal, sera reemplazada en los distintos servicios'''
         db = session()
         try:
-            climsg = json.loads(climsg)
-            catalogo = db.query(Bodega).all()
-                
-            return "Grupo creado con id: " + catalogo
+            #climsg = json.loads(climsg)
+            catalogo = db.query(Bodega).filter(Bodega.stock > 0).all()
+            print("Lista de productos:")
+            for c in catalogo:
+                print("#ID: "+str(c.id)+" Nombre: "+str(c.nombre_producto)+" Precio: "+str(c.valor_unidad)+" Stock: "+str(c.stock))
+            return "Visualizacion de catalogo de productos"
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
---
+++ backend/clicatalogo.py
@@ -0,0 +1,15 @@
+from clients.Client import Client
+from getpass import getpass
+import json
+        
+if __name__ == "__main__":
+    print("Service: Catalogo")
+    keep_alive = True
+    try:    
+        a = Client("bgrup")
+        msg = a.exec_client(debug=True, climsg="Hola")
+        print("###################################\n\n", msg, "\n\n###################################")
+    except KeyboardInterrupt:
+        print("\nCerrando cliente, hasta pronto ....")
+        keep_alive = False
+        exit()
