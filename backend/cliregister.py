97a95f669fd7b8625d204c6e4c59389367bdd201
Joaquín Fernández <67400456+Joacker@users.noreply.github.com>
2022-12-11 00:02:21 -0300

    add set and ready

--- backend/cliregister.py
+++ backend/cliregister.py
@@ -7,23 +7,41 @@ if __name__ == "__main__":
     keep_alive = True
     try:
         while(keep_alive):
-            user = input("Ingrese usuario: ")
-            password = getpass("Ingrese contraseña: ")
-            email = input("Ingrese email: ")
-            phone = input("Ingrese telefono: ")
-            
-            try: 
-                climsg = {
-                    "user": user,
-                    "password": password,
-                    "email": email,
-                    "phone": phone
-                }
-                a = Client("bregi")
-                msg = a.exec_client(debug=True, climsg=json.dumps(climsg))
-                print("###################################\n\n", msg, "\n\n###################################")
-            except Exception as e:
-                print("Error: ", e)
+            print("¿Tiene alguna cotizacion actual para añadir productos?")
+            print("(1) Sí")
+            print("(2) No")
+            opcion = input("Ingrese opcion: ")
+            if (opcion == "1"):
+                try:
+                    token = getpass("Token: ")
+                    nombre_producto = input("Ingrese nombre producto: ")
+                    cantidad = int(input("Ingrese cantidad: "))
+                    climsg = {
+                        "token": token,
+                        "nombre_producto": nombre_producto,
+                        "cantidad": cantidad
+                    }
+                    a = Client("bregi")
+                    msg = a.exec_client(debug=True, climsg=json.dumps(climsg))                  
+                    print("###################################\n\n", msg, "\n\n###################################")
+                except Exception as e:
+                   print("Error: ", e)
+                    
+            if (opcion == "2"):
+                try:
+                    token = getpass("Token: ")
+                    nombre_producto = input("Ingrese nombre producto: ")
+                    cantidad = int(input("Ingrese cantidad: "))
+                    climsg = {
+                        "token": token,
+                        "nombre_producto": nombre_producto,
+                        "cantidad": cantidad
+                    }
+                    a = Client("bregi")
+                    msg = a.exec_client(debug=True, climsg=json.dumps(climsg))                  
+                    print("###################################\n\n", msg, "\n\n###################################")
+                except Exception as e:
+                   print("Error: ", e)
     except KeyboardInterrupt:
         print("\nCerrando cliente, hasta pronto ....")
         keep_alive = False
