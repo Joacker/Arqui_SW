50a42567f6b3ef39c9790952c743c56dc91de426
Nicolás Moncada <69024747+NicoMonc@users.noreply.github.com>
2022-12-15 12:12:12 -0300

    Cambios

--- backend/compra.py
+++ backend/compra.py
@@ -29,9 +29,11 @@ class Compra(Service):
                 rut_i = climsg["rut"]
                 medio_pago_i = climsg["medio_pago"]
                 find_product_to_update =  db.query(Bodega, Producto).join(Producto).filter(Producto.cotizacion_id == valid_cotizacion.id).all()
+                cotiid=0
                 suma = 0
                 for bodega, producto in find_product_to_update:
                     #print(producto.bodega_id, producto.cantidad, bodega.nombre_producto, bodega.valor_unidad)
+                    cotiid=producto.cotizacion_id
                     suma = suma + (producto.cantidad * bodega.valor_unidad)
                 valid_boleta, cotiza = db.query(Boleta, Cotizacion).join(Cotizacion
                                                                          ).filter(
@@ -41,10 +43,22 @@ class Compra(Service):
                                                                             ).first()
                 valid_boleta.rut = rut_i
                 db.commit()
+                
                 print(valid_boleta.rut)
                 fecha_i = datetime.datetime.now()
                 if(cotiza.concludes == 0):
                     if (medio_pago_i == "efectivo"):
+                        prodcoti= db.execute("SELECT * FROM producto where cotizacion_id="+str(cotiid)).fetchall()
+                        for produni in prodcoti:
+                            restar=produni.cantidad
+                            buscar=produni.bodega_id
+                            afectadas=db.execute("UPDATE bodega SET stock = "+str(restar)+"WHERE id="+str(buscar))
+                            #Agregar stock automatico con stock final <5
+                            if afectadas:
+                                print("Stock ajustado")
+                                db.commit()
+                            else:
+                                print("Error")
                         valid_boleta.rut = rut_i ; db.commit()
                         valid_boleta.mediopago = "efectivo" ; db.commit()
                         valid_boleta.fecha = fecha_i ; db.commit()
@@ -57,6 +71,16 @@ class Compra(Service):
                         suma = 0
                         return "Compra realizada con efectivo"
                     elif (medio_pago_i == "debito"):
+                        prodcoti= db.execute("SELECT * FROM producto where cotizacion_id="+str(cotiid)).fetchall()
+                        for produni in prodcoti:
+                            restar=produni.cantidad
+                            buscar=produni.bodega_id
+                            afectadas=db.execute("UPDATE bodega SET stock = "+str(restar)+"WHERE id="+str(buscar))
+                            if afectadas:
+                                print("Stock ajustado")
+                                db.commit()
+                            else:
+                                print("Error")
                         valid_boleta.rut = rut_i; db.commit()
                         valid_boleta.mediopago = "debito"; db.commit()
                         valid_boleta.fecha = fecha_i ; db.commit()
@@ -69,6 +93,22 @@ class Compra(Service):
                         suma = 0
                         return "Compra realizada con debito"
                     elif (medio_pago_i == "credito"):
+                        prodcoti= db.execute("SELECT * FROM producto where cotizacion_id="+str(cotiid)).fetchall()
+                        for produni in prodcoti:
+                            restar=produni.cantidad
+                            buscar=produni.bodega_id
+                            afectadas=db.execute("UPDATE bodega SET stock = "+str(restar)+"WHERE id="+str(buscar))
+                            valid_stock = db.query(Bodega).filter(Bodega.id == buscar).first()
+                            if (valid_stock.stock < 5):
+                                stock_final = valid_stock.stock + 10
+                                valid_stock.stock = stock_final
+                                print("Stock agregado")
+                                db.commit()
+                            if afectadas:
+                                print("Stock ajustado")
+                                db.commit()
+                            else:
+                                print("Error")
                         valid_boleta.rut = rut_i; db.commit()
                         valid_boleta.mediopago = "credito"; db.commit()
                         valid_boleta.fecha = fecha_i; db.commit()
